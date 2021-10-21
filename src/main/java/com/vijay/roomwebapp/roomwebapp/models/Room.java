f37da731917e76ad82235a620d4723356731f627
Vijay P V <pvvijayraj1998@gmail.com>
2021-10-21 14:26:29 +0530

    initial commit

---
+++ src/main/java/com/vijay/roomwebapp/roomwebapp/models/Room.java
@@ -0,0 +1,52 @@
+package com.vijay.roomwebapp.roomwebapp.models;
+
+public class Room {
+    private long id;
+    private String number;
+
+    public Room(long id, String number, String name, String info) {
+        this.id = id;
+        this.number = number;
+        this.name = name;
+        this.info = info;
+    }
+
+    public Room() {
+    }
+
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    private String info;
+}
---
+++ src/test/java/com/vijay/roomwebapp/roomwebapp/RoomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.roomwebapp.roomwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
