93d6c6824476221828fd1fd09035475841ac4f11
Vijay P V <pvvijayraj1998@gmail.com>
2021-10-25 17:44:03 +0530

    Added security and login

---
+++ src/main/java/com/vijay/roomwebapp/roomwebapp/controllers/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.vijay.roomwebapp.roomwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+
+    @GetMapping("/login")
+    public  String getLogin() {
+        return "login";
+    }
+}
